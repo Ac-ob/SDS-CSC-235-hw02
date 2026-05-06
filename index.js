4ccc1b1645b5c55f87338d3afe2186bb73ea1bc0
Ac-ob <157826104+Ac-ob@users.noreply.github.com>
2026-05-06 19:42:20 -0400

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-function mouseOver() {
-	document.getElementById("color").classList.add("highlight"); 
-}
-
-function mouseOut() {
-	document.getElementById("color").classList.remove("highlight"); 
-}
